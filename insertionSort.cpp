6465040e0ca6f74e70e0bba2bc235e7688550f9b
Satori000 <Satori000@users.noreply.github.com>
2016-01-27 09:45:23 -0500

    Updated insertion sort

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -15,12 +15,10 @@ int main(){
 
   }
 
-
-
   //loop goes through inserting every element
-  /*for(int i = 1; i < N; ++i)
+  for(int i = 1; i < N; ++i)
   {
-    //t is the element being inserted
+   //t is the element being inserted
    int t = a[i];
    //j is the index which decides the insertion point
    int j;
@@ -33,24 +31,7 @@ int main(){
   //inserts element
    a[j + 1] = t;
 
- } */
-
-
-  for(int i = 0; i < N; ++i){
-    int insertion = a[i];
-
-    int j;
-
-    for(j = i + 1; j < N && a[j] < insertion; ++j){
-      a[j-1] = a[j];
-
-
-    }
-    a[j-1] = insertion;
-
-  }
-  // 8 5 4 23 1 5
-  //12 19 16 17 13
+ } 
 
   //prints the sorted array
   for(int i = 0; i < N; ++i){
